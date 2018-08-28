0f73aa9038b2845bf5ca32786f487e015db7fe53
rajumca <rajugriet483@gmail.com>
2018-08-27 23:59:40 -0500

    follow unfollow not in sync

--- src/main/java/com/rajusays/demo/controller/UserController.java
+++ src/main/java/com/rajusays/demo/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.rajusays.demo.controller;
 
 import java.util.Collections;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
